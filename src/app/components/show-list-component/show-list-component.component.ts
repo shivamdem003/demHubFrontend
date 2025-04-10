9735ca59e889b6b08d24e3d668f5945023f26f5e
shivamdem003 <1998.saha.shivam@gmail.com>
2025-04-10 18:00:56 +0530

    adding extra chnages to the UI

--- src/app/components/show-list-component/show-list-component.component.ts
+++ src/app/components/show-list-component/show-list-component.component.ts
@@ -18,6 +18,10 @@ export class ShowListComponentComponent implements OnInit{
   showAll: boolean = false;
   initialLimit: number = 9;
 
+  slideshowShows: FileNode[] = [];
+  remainingShows: FileNode[] = [];
+  currentSlide = 0;
+
   constructor(private videoService: VideoService, private router: Router) {}
 
   ngOnInit(): void {
@@ -25,6 +29,24 @@ export class ShowListComponentComponent implements OnInit{
       this.shows = (data.children || []).filter(item => item.folder);
       this.updateVisibleShows();
     });
+
+    this.videoService.getFileStructure('').subscribe(data => {
+      const folders = (data.children || []).filter(item => item.folder);
+      this.shows = folders;
+  
+      // Randomize and split
+      const shuffled = folders.sort(() => 0.5 - Math.random());
+      this.slideshowShows = shuffled.slice(0, 4);
+      this.remainingShows = shuffled.slice(4);
+    });
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slideshowShows.length;
+  }
+  
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slideshowShows.length) % this.slideshowShows.length;
   }
 
   updateVisibleShows(): void {
